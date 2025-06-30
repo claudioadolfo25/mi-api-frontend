157d488c2d4ea8ade97191d44868c2c523b692ae
claudioadolfo25 <ayelef.adolfo@gmail.com>
2025-06-30 00:35:08 -0400

    Delete page.tsx

--- page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <main className='min-h-screen flex flex-col items-center justify-center'>
-      <h1 className='text-4xl font-bold text-center'>
-        Bienvenido a mi aplicación con Auth0
-      </h1>
-    </main>
-  );
-}
