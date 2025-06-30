fe684c59ccadbf655a888f91fb82e429362d23dd
claudioadolfo25 <ayelef.adolfo@gmail.com>
2025-06-30 01:37:37 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 export default function Home() {
   return (
-    <main className='min-h-screen flex flex-col items-center justify-center'>
-      <h1 className='text-4xl font-bold text-center'>
-        Bienvenido a mi aplicación con Auth0
+    <main className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-center mb-4">
+        ¡Bienvenido a QMSNCH!
       </h1>
+      <p className="text-lg text-gray-600">
+        Backend funcionando en <a href="http://localhost:3001" className="text-blue-500 hover:underline">http://localhost:3001</a>
+      </p>
     </main>
   );
 }
