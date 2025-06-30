5fd159ae79ad615707e58fe69b05906082c0c8dd
claudioadolfo25 <ayelef.adolfo@gmail.com>
2025-06-30 01:39:57 -0400

    Create login-button.tsx

---
+++ src/app/login-button.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { useAuth0 } from '@auth0/auth0-react';
+
+export default function LoginButton() {
+  const { loginWithRedirect } = useAuth0();
+
+  return (
+    <button 
+      onClick={() => loginWithRedirect()}
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+    >
+      Iniciar sesión
+    </button>
+  );
+}
