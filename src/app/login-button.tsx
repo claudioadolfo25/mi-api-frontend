d3fa72fdcbebc00b26df726940d905418320cafe
claudioadolfo25 <ayelef.adolfo@gmail.com>
2025-07-01 17:17:50 -0400

    Update login-button.tsx

--- src/app/login-button.tsx
+++ src/app/login-button.tsx
@@ -1,3 +1,4 @@
+// src/app/login-button.tsx
 'use client';
 
 import { useAuth0 } from '@auth0/auth0-react';
@@ -5,10 +6,20 @@ import { useAuth0 } from '@auth0/auth0-react';
 export default function LoginButton() {
   const { loginWithRedirect } = useAuth0();
 
+  const handleLogin = () => {
+    loginWithRedirect({
+      authorizationParams: {
+        // Usar variable de entorno para audience
+        audience: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE!,
+        redirect_uri: window.location.origin
+      }
+    });
+  };
+
   return (
-    <button 
-      onClick={() => loginWithRedirect()}
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+    <button
+      onClick={handleLogin}
+      className="bg-qmsnch-primary hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-full transition duration-300"
     >
       Iniciar sesión
     </button>
